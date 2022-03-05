a8a0225b93a330456a2c86eb240ee60f4bc3a9b3
Zhayu517 <zhangzy517@gmail.com>
2022-03-05 20:12:36 +0000

    update get_embvec()

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,9 +3,14 @@ import torch
 import torch.nn as nn
 import configparser
 
+parser = configparser.ConfigParser()
+parser.sections()
+
 def get_embvec(sentence, vol, embedding_dim):
+    model_type = parser['Options for model']['model']
     words = sentence.split()
-    words = list(filter(lambda token: token not in string.punctuation, words))
+    if model_type == 'bow':
+        words = list(filter(lambda token: token not in string.punctuation, words))
     vec = torch.FloatTensor(len(words), embedding_dim)
     for i in range(0, len(words)):
         if words[i] in vol.keys():
@@ -14,6 +19,3 @@ def get_embvec(sentence, vol, embedding_dim):
             vec[i] = (vol.get("#UNK#"))
 
     return vec
-
-parser = configparser.ConfigParser()
-parser.sections()
